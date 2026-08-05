30e8959ad8b25959a8d9b2ab66deeca9e4d186fb
aaaaaayves <nkj29k8vnw@privaterelay.appleid.com>
2026-08-05 18:02:03 +0900

    Add files via upload

---
+++ 260803.cpp
@@ -0,0 +1,25 @@
+//# include <stdio.h>
+//
+//void main()
+//{
+//	printf(" %d ", 100 - 99);
+//}
+
+//#include <stdio.h>
+//
+//void main()
+//{
+//	printf("\n");
+//	printf("%d", 100 + 100);
+//	printf("\n");
+//}
+
+#include <stdio.h>
+
+void main()
+{
+	printf("%d", 100, 200);
+	printf("\n");
+	printf("%d %d", 100);
+	printf("\n");
+}
