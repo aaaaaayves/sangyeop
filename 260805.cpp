aa3208f8394c018cb72f43cb2c533aac0adb4f99
aaaaaayves <nkj29k8vnw@privaterelay.appleid.com>
2026-08-05 19:51:20 +0900

    Add files via upload

---
+++ 260805.cpp
@@ -0,0 +1,153 @@
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("안녕하세요\n");
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("안녕하세요");
+//	return 0;
+//}
+
+//#include <stdio.h>	
+//
+//int main()
+//{
+//	printf("안녕하세요\n안녕하세요안녕하세요\n");
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("안녕하세요\n");
+//	printf("안녕하세요");
+//	printf("안녕하세요\n");
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("사과 1개\n");
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int apple = 1;
+//	printf("사과 %d개\n", apple);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("키보드 3개\n");
+//	printf("마우스 2개\n");
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int keyboard = 3;
+//	int mouse = 2;
+//	printf("키보드: %d개\n", keyboard);
+//	printf("마우스: %d개\n", mouse);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int coin1 = 500;
+//	int coin2 = 1000;
+//	printf("%d + %d = %d\n", coin1, coin2, coin1 + coin2);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	char A;
+//	A = '1';
+//	printf("A = %c, A = %d\n", A, A);
+//	return 0;
+//
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	double pi;
+//	pi = 3.14;
+//	float e = 2.718;
+//	printf("pi = %.2lf, e = %.4f.. \n", pi, e);
+//	return 0;
+//
+//}
+
+//#include <stdio.h>
+////반지름의 길이가 5인 원의넓이와 원의 둘레를 구하여 출력하라
+//int main()
+//{
+//	char radius; //반지름
+//	radius = 5;
+//
+//	double pi; //원주율
+//	pi = 3.14;
+//
+//	float area; //넓이
+//	area = radius * radius * pi;
+//
+//	float diameter; //지름
+//	diameter = radius * 2 * pi;
+//
+//	
+//	
+//	printf("원의넓이: %.1f\n", area);
+//	printf("원의둘레: %.2f", diameter);
+//	//반지름*반지름*원주율 = 넓이
+//	//원의 둘레 지름 x 원주율 
+//	return 0;
+//
+//}
+
+//#include <stdio.h>
+////밑변의 길이가 3 윗변의 길이 5 높이 7 사다리꼴의 넓이 구하기 변수 4개선언
+//int main()
+//{
+//	char bs;
+//	bs = 3;
+//
+//	char us;
+//	us = 5;
+//
+//	char high;
+//	high = 7;
+//
+//	float area;
+//	area = (us + bs) * high / 2;
+//
+//	printf("사다리꼴의 넓이: %.1f", area);
+//
+//
+//	return 0;
+//
+//}
+
